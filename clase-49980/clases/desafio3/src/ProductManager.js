fb68f707c983500d07ca6d60ceab00d414b62fe4
rigatalarcon <ricardo.alarcon82@gmail.com>
2023-10-31 18:40:21 -0300

    arreglo en desafio3

--- clase-49980/clases/desafio3/src/ProductManager.js
+++ clase-49980/clases/desafio3/src/ProductManager.js
@@ -3,10 +3,16 @@ import fs from 'fs/promises'
 export class ProductManager {
     constructor(ruta) {
         this.ruta = ruta
-    } 
+    }
 
-async getALL () {
-    const json = await fs.readFile(this.ruta, 'utf-8')
-    return JSON.parse(json)
+    async getALL() {
+        const json = await fs.readFile(this.ruta, 'utf-8')
+        return JSON.parse(json)
+    }
+    async getById(id) {
+        const json = await fs.readFile(this.ruta, 'utf-8')
+        const buscado = productos.find(p => p.id === id)
+        if (!buscado) throw new Error(`no se encontro el producto con id ${id}`)
+        return buscado
     }
 }
--- clase-49980/clases/desafio3/src/main.js
+++ clase-49980/clases/desafio3/src/main.js
@@ -1,14 +1,29 @@
 import express from 'express'
-import { ProductManager } from './ProductManager'
-import { PORT, PRODUCT_JSON } from './config'
+import { ProductManager } from './ProductManager.js'
+import { PORT, PRODUCT_JSON } from './config.js'
 
-const pm = ProductManager(PRODUCT_JSON)
+const pm = new ProductManager(PRODUCT_JSON)
 
 const app = express()
 
 app.get('../productos', async (req, res) => {
+    const stock = parseInt(req.query.stock)
+
     try {
-        const productos = await pm.getALL()
+        const productos = await pm.getALL({stock})
+        res.json(productos)
+
+    } catch (error) {
+        res.json({
+            status: 'error',
+            message: error.message
+        })
+    }
+})
+app.get('../productos/:id', async (req, res) => {
+    const id = parseInt(req.params.id)
+    try {
+        const productos = await pm.getById(id)
         res.json(productos)
 
     } catch (error) {
